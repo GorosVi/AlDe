29ecb65dce86e24bf4f09926d16db7414c9344a3
GorosVia <gorosvia@gmail.com>
2014-04-27 11:24:01 +0400

    Screen resolution changed to 1024x768 @ 60Hz

--- VGAOut.vhd
+++ VGAOut.vhd
@@ -21,22 +21,22 @@ end entity;
 
 architecture Behavioral of VOut is
 	
-	-- 800x600 @ 60Hz, 40mHz pixel clock - params from tinyvga.com/vga-timing
+	-- 1024x768 @ 60Hz, 65mHz pixel clock - params from tinyvga.com/vga-timing
 
-	constant HorVisArea : INTEGER := 800;
-	constant HorFrPArea : INTEGER := 40;
-	constant HorSyPArea : INTEGER := 128;
-	constant HorBkPArea : INTEGER := 88;
+	constant HorVisArea : INTEGER := 1024;
+	constant HorFrPArea : INTEGER := 24;
+	constant HorSyPArea : INTEGER := 136;
+	constant HorBkPArea : INTEGER := 160;
 	constant HorTotArea : INTEGER := HorVisArea + HorFrPArea + HorSyPArea + HorBkPArea;
 
-	constant VerVisArea : INTEGER := 600;
-	constant VerFrPArea : INTEGER := 1;
-	constant VerSyPArea : INTEGER := 4;
-	constant VerBkPArea : INTEGER := 23;
+	constant VerVisArea : INTEGER := 768;
+	constant VerFrPArea : INTEGER := 3;
+	constant VerSyPArea : INTEGER := 6;
+	constant VerBkPArea : INTEGER := 29;
 	constant VerTotArea : INTEGER := VerVisArea + VerFrPArea + VerSyPArea + VerBkPArea;
 
 	signal color : STD_LOGIC;
-	signal mpos  : INTEGER range 0 to 4020;	
+	signal mpos  : INTEGER range 0 to 8979;	
 	signal CCNT  : INTEGER range 0 to HorTotArea;
 	signal RCNT  : INTEGER range 0 to VerTotArea;
 	signal cctmp : INTEGER range 0 to VerVisArea;
@@ -45,7 +45,7 @@ architecture Behavioral of VOut is
 	component thpic is
 		port (
 			clk :  in STD_LOGIC;
-			adr :  in STD_LOGIC_VECTOR (12 downto 0);
+			adr :  in STD_LOGIC_VECTOR (13 downto 0);
 			q   : out STD_LOGIC_VECTOR (7  downto 0)
 		);
 	end component;
@@ -53,7 +53,7 @@ architecture Behavioral of VOut is
 begin
 
 	TPic:	thpic port map (
-			adr => conv_std_logic_vector(mpos,13),
+			adr => conv_std_logic_vector(mpos,14),
 			clk => clk,
 			q   => picv
 	);
@@ -62,12 +62,13 @@ begin
 	begin
 	
 	if rising_edge(clk) then
-		
-		-- Test frame
-		--if (CCNT = 0 or RCNT = 0 or CCNT = HorVisArea - 1 or RCNT = VerVisArea - 1) and
-		--   (CCNT < HorVisArea) and (RCNT < VerVisArea) then	RCH <= b"1111"; GCH <= b"1111"; 
-		--    BCH <= b"1111"; else RCH <= b"0000"; GCH <= b"0000"; BCH <= b"0000"; end if;
 
+		-- Margins test frame
+--		if  conv_std_logic_vector(CCNT,1) = b"0" and conv_std_logic_vector(RCNT,1) = b"0" and
+--			(CCNT = 0 or RCNT = 0 or CCNT = HorVisArea - 1 or RCNT = VerVisArea - 1) and
+--			 CCNT < HorVisArea and RCNT < VerVisArea
+--		    	then RCH <= x"F"; GCH <= x"F"; BCH <= x"F"; else RCH <= x"0"; GCH <= x"0"; BCH <= x"0";
+--		end if;
 
 		-- Visible area control
 		if CCNT < HorVisArea and RCNT < VerVisArea then
@@ -101,17 +102,17 @@ begin
 		end if;
 
 
-		-- Sync pulse generation
+		-- Sync pulse generation - negative polarity
 		if (CCNT >= HorVisArea + HorFrPArea) and (CCNT < HorTotArea - HorBkPArea) then
-			HS <= '1';
-		else
 			HS <= '0';
+		else
+			HS <= '1';
 		end if;
 
 		if (RCNT >= VerVisArea + VerFrPArea) and (RCNT < VerTotArea - VerBkPArea) then
-			VS <= '1';
-		else
 			VS <= '0';
+		else
+			VS <= '1';
 		end if;
 
 	end if;
@@ -139,8 +140,8 @@ process(clk)
 		end if;
 
 		-- Clear memory position each new frame
-		if (CCNT = HorTotArea - 2) and ((RCNT = VerTotArea - 1) or (RCNT < 140)) then
-			mpos <= 1;
+		if (CCNT = HorTotArea - 2) and (RCNT = VerTotArea - 1) then
+			mpos <= 1; -- at 0 in memory placed number of blank rows (not used)
 		end if;
 
 	end if;
@@ -164,7 +165,7 @@ use ALTERA.ALTERA_SYN_ATTRIBUTES.ALL;
 entity thpic is
 	port (
 		clk :  in STD_LOGIC;
-		adr :  in STD_LOGIC_VECTOR (12 downto 0);
+		adr :  in STD_LOGIC_VECTOR (13 downto 0);
 		q   : out STD_LOGIC_VECTOR (7  downto 0)
 	);
 end thpic;
@@ -173,7 +174,7 @@ end thpic;
 
 architecture rtl of thpic is
 	
-	type mem_t is array (4020 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
+	type mem_t is array (8979 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
 		signal    rom: mem_t;
 		attribute ram_init_file: string;
 		attribute ram_init_file  of rom: signal is "TPic.mif";
